43af5009667e305cd8ff8cf08c74c7249403105c
eron2023 <erondauti880@gmail.com>
2023-12-06 20:33:46 +0100

    ndryshim ne login,faqja-kryesore,krijim i regjistrit, ndryshim ne skriptin,ndryshim ne css

--- skripti.js
+++ skripti.js
@@ -43,7 +43,120 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
 });
- 
+ // Rregjistrimi
+ document.addEventListener('DOMContentLoaded', function () {
+  let emailRegex = /^[a-zA-Z.-_]+@[a-z]+\.[a-z]{2,3}$/;
+  let passwordRegex = /^(?=.*[a-zA-Z])(?=.*[A-Z])(?=.*\d).{4,20}$/;
+  let nameRegex = /^[a-zA-Z]+$/;
+  let phoneRegex = /^\d{4,10}$/; 
+  function validateRegisterForm() {
+    let nameInput = document.getElementById('name');
+    let surnameInput = document.getElementById('surname');
+    let phoneInput = document.getElementById('phone');
+    let emailInput = document.getElementById('email');
+    let passwordInput = document.getElementById('password');
+
+    let nameError = document.getElementById('nameError');
+    let surnameError = document.getElementById('surnameError');
+    let phoneError = document.getElementById('phoneError');
+    let emailError = document.getElementById('emailError');
+    let passwordError = document.getElementById('passwordError');
+
+    nameError.innerText = '';
+    surnameError.innerText = '';
+    phoneError.innerText = '';
+    emailError.innerText = '';
+    passwordError.innerText = '';
+
+    if (nameInput.value.trim() === '' || !nameRegex.test(nameInput.value.trim())) {
+      nameError.innerText = 'Ju lutem shenoni emrin tuaj ne menyre valide.';
+      return;
+    } else {
+      nameError.innerText = '';
+    }
+
+    if (surnameInput.value.trim() === '' || !nameRegex.test(surnameInput.value.trim())) {
+      surnameError.innerText = 'Ju lutem shenoni mbiemrin tuaj ne menyre valide.';
+      return;
+    } else {
+      surnameError.innerText = '';
+    }
+
+    if (phoneInput.value.trim() === '' || !phoneRegex.test(phoneInput.value.trim())) {
+      phoneError.innerText = 'Ju lutem shenoni numrin tuaj të telefonit ne menyre valide.';
+      return;
+    } else {
+      phoneError.innerText = '';
+    }
+
+    if (!emailRegex.test(emailInput.value)) {
+      emailError.innerText = 'Formati i Emailit eshte i gabuar.';
+      return;
+    } else {
+      emailError.innerText = '';
+    }
+
+    if (!passwordRegex.test(passwordInput.value)) {
+      passwordError.innerText = 'Ju lutem shenoni nje shkronje te madhe se pari dhe nje numer.';
+      return;
+    } else {
+      passwordError.innerText = '';
+    }
+
+    alert('Jeni regjistruar me sukses!');
+    window.location.href = 'faqja-kryesore.html';
+  }
+
+  function navigateOnArrowPress(event, currentElement, nextElementId, previousElementId) {
+    if (event.key === 'ArrowDown') {
+      document.getElementById(nextElementId).focus();
+    } else if (event.key === 'ArrowUp') {
+      document.getElementById(previousElementId).focus();
+    }
+  }
+
+  function handleEnterKey(event) {
+    if (event.key === 'Enter') {
+      validateRegisterForm();
+    }
+  }
+
+  document.getElementById('name').addEventListener('blur', validateRegisterForm);
+  document.getElementById('surname').addEventListener('blur', validateRegisterForm);
+  document.getElementById('phone').addEventListener('blur', validateRegisterForm);
+  document.getElementById('email').addEventListener('blur', validateRegisterForm);
+  document.getElementById('password').addEventListener('blur', validateRegisterForm);
+
+  document.getElementById('name').addEventListener('keydown', function (event) {
+    navigateOnArrowPress(event, this, 'surname', 'password');
+    handleEnterKey(event);
+  });
+
+  document.getElementById('surname').addEventListener('keydown', function (event) {
+    navigateOnArrowPress(event, this, 'phone', 'name');
+    handleEnterKey(event);
+  });
+
+  document.getElementById('phone').addEventListener('keydown', function (event) {
+    navigateOnArrowPress(event, this, 'email', 'surname');
+    handleEnterKey(event);
+  });
+
+  document.getElementById('email').addEventListener('keydown', function (event) {
+    navigateOnArrowPress(event, this, 'password', 'phone');
+    handleEnterKey(event);
+  });
+
+  document.getElementById('password').addEventListener('keydown', function (event) {
+    navigateOnArrowPress(event, this, 'name', 'email');
+    handleEnterKey(event);
+  });
+
+  document.getElementById('registerButton').addEventListener('click', validateRegisterForm);
+});
+
+
+
 /*Kontakti*/
 
 document.addEventListener('DOMContentLoaded', function () {
